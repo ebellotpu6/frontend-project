f1e6322f056a2749783a814003d8f394b5cfee07
ebellotpu6 <ebellotpu6@gmail.com>
2022-06-07 11:54:37 +0200

    Structuring the project

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import './index.css'
+import { Home, MovieDetails } from './screens/index';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}>
+          <Route path="/movie/:movieId" element={<MovieDetails />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 )
